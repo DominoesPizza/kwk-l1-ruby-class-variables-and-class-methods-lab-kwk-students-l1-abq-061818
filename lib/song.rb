4d4aac05d4776ed7eca92fe54520f79fdd1b1ff2
DomIsNotOnFire <djpamcake38@gmail.com>
2018-06-22 19:04:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,14 @@
+class Album
 
+  def release_date = (date)
+    @release_date = (date)
+    
+  def release_date
+    @release_date
+  end
+
+end
+
+shawn = Album.new
+shawn.release_date = 2018
+puts album.release_date
